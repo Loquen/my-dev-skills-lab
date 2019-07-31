557943cbdd80ccfaff19ff24d506ecd3db434eed
Loquen Jones <lrjones416@gmail.com>
2019-07-31 14:49:48 -0700

    final commit working with local storage

--- js/main.js
+++ js/main.js
@@ -1,13 +1,16 @@
+for(var i = 0; i < localStorage.length; i++){
+    $('ul').append(`<li><span>x</span>${localStorage.getItem(localStorage.key(i))}</li>`);
+}
 
 // Remove existing items
 $('ul').on('click','li', function(){
-    //console.log(evt);
     $(this).remove();
 });
 
 $('button').on('click', function(evt){
-    //evt.preventDefault();
     let input = $('input').val();
-    $('ul').append(`<br><li><span>x</span>${input}<li>`);
-    $(input).val('');
+    $('ul').append(`<li><span>x</span>${input}</li>`);
+    localStorage.setItem(input, input);
+    //console.log(localStorage);
+    //$(input).val('');
 });
